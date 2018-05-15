c4f5f857df68412c96780b89b8397517906426c0
Marly Cormar <marlycormar@gmail.com>
2018-05-15 10:17:33 -0400

    Add module template.

---
+++ ExternalModule.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Provides ExternalModule class for SuspensionWarning.
+ */
+
+namespace SuspensionWarning\ExternalModule;
+
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+/**
+ * ExternalModule class for SuspensionWarning.
+ */
+class ExternalModule extends AbstractExternalModule {
+
+}
