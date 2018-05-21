3ea217d7d2a299b401eded58b0f8f8442bbc30cd
Marly Cormar <marlycormar@gmail.com>
2018-05-21 13:38:01 -0400

    Include account activation script.

---
+++ activate_account.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * Allows user to update both user_lastactivity and user_lastlogin
+ */
+
+define('NOAUTH',true);
+if (!file_exists('../../redcap_connect.php')) 
+    require_once "/var/www/redcap/redcap_connect.php";
+else 
+    require_once '../../redcap_connect.php';
+
+$username = $_GET["username"];
+
+if($_GET["username"] == "") 
+	print_r("We are unable to extend your account suspension time. Please contact the REDCap Support Team.");
+
+else {
+	$sql = "update redcap_user_information set user_lastactivity = NOW(),	 user_lastlogin = NOW() where username ='$username';";
+
+	db_query($sql);
+	
+	print_r("Your account suspension time has been succesfully extended.");
+}
